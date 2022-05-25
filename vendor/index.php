8dfcb5ada216a558a0dcd82a95d4472f61c0a70b
Andrea Marchetto <adremasu@gmail.com>
2022-05-25 20:49:04 +0200

    initial commit

---
+++ vendor/index.php
@@ -0,0 +1,46 @@
+<?php
+// Require Composer's autoloader.
+require 'vendor/autoload.php';
+echo 'ciao';
+// Using Medoo namespace.
+use Medoo\Medoo;
+
+$database = new Medoo([
+  // [required]
+  'type' => 'mysql',
+  'host' => 'localhost',
+  'database' => 'qdc',
+  'username' => 'root',
+  'password' => 'marez123',
+
+  // [optional]
+  'charset' => 'utf8mb4',
+  'collation' => 'utf8mb4_general_ci',
+  'port' => 3306,
+
+  // [optional] The table prefix. All table names will be prefixed as PREFIX_table.
+  'prefix' => 'PREFIX_',
+
+  // [optional] To enable logging. It is disabled by default for better performance.
+  'logging' => true,
+
+  // [optional]
+  // Error mode
+  // Error handling strategies when the error is occurred.
+  // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+  // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+  'error' => PDO::ERRMODE_SILENT,
+
+  // [optional]
+  // The driver_option for connection.
+  // Read more from http://www.php.net/manual/en/pdo.setattribute.php.
+  'option' => [
+    PDO::ATTR_CASE => PDO::CASE_NATURAL
+  ],
+
+  // [optional] Medoo will execute those commands after the database is connected.
+  'command' => [
+    'SET SQL_MODE=ANSI_QUOTES'
+  ]
+]);
+?>
